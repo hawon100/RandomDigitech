ccd15a87b0e2bafb68903b93b2f0ab58b945d8fb
janghyuck0909 <sdh230213@sdh.hs.kr>
2023-07-22 22:16:06 +0900

    save
    save

---
+++ Assets/janghyuck/Script/GameScenesMove.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameScenesMove : MonoBehaviour
+{
+    public void GameScenesCtrl()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void GameScenesCtrlBack()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
